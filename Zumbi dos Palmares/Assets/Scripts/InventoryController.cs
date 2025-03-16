39b4ee775f60d2fbffde82f17944665e30cc674b
PhelipeTamagno <phelipetamagno@gmail.com>
2025-03-16 03:00:53 -0300

    Inventario, HotBar

---
+++ Zumbi dos Palmares/Assets/Scripts/InventoryController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryController : MonoBehaviour
+{
+    public GameObject inventoryPanel;
+    public GameObject slotPrefab;
+    public int slotCount;
+    public GameObject[] itemPrefabs;
+
+    private bool isInventoryOpen = false; // Estado do inventário
+
+    void Start()
+    {
+        inventoryPanel.SetActive(false); // Garante que o inventário inicie fechado
+
+        for (int i = 0; i < slotCount; i++)
+        {
+            Slot slot = Instantiate(slotPrefab, inventoryPanel.transform).GetComponent<Slot>();
+            if (i < itemPrefabs.Length)
+            {
+                GameObject item = Instantiate(itemPrefabs[i], slot.transform);
+                item.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+                slot.currentItem = item;
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            ToggleInventory();
+        }
+    }
+
+    void ToggleInventory()
+    {
+        isInventoryOpen = !isInventoryOpen; // Alterna o estado do inventário
+        inventoryPanel.SetActive(isInventoryOpen);
+    }
+}
