733cb7b8ba4996119b91b4edaa97e223a79d15b6
Schlagel <e27544208@gmail.com>
2025-03-15 22:43:44 -0300

    Revert "nao sei"
    This reverts commit 5f34a2272357a0c37d4fe10cf366358d23f797b9, reversing
    changes made to 53fa321c6b7d71b66b491ab7f5be7694617e406c.

--- Zumbi dos Palmares/Assets/Scripts/PlayerMovement.cs
+++ Zumbi dos Palmares/Assets/Scripts/PlayerMovement.cs
@@ -3,81 +3,64 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    private float playerInitialSpeed;
-    private bool isAttack = false;
 
-    private Rigidbody2D rb;
     private Animator anim;
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
     private Vector2 movement;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
-        playerInitialSpeed = moveSpeed;
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-        ProcessInput();
-        Animate();
-        Flip();
-        OnAttack();
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        movement = movement.normalized;
     }
 
     void FixedUpdate()
     {
-        Move();
-    }
-
-    void ProcessInput()
-    {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-        movement = movement.normalized;
-    }
-
-    void Move()
-    {
         rb.velocity = movement * moveSpeed;
-    }
 
-    void Animate()
-    {
-        if (isAttack)
+        if (movement.x != 0)
         {
-            anim.SetInteger("Movimento", 2);
+            anim.SetLayerWeight(0, 0);
+            anim.SetLayerWeight(2, 1);
+
+            // Inverte o sprite dependendo da direção
+            sprite.flipX = movement.x < 0;
+        }
+
+        if (movement.y > 0 && movement.x == 0)
+        {
+            anim.SetLayerWeight(2, 0);
+            anim.SetLayerWeight(1, 1);
         }
         else
         {
-            anim.SetInteger("Movimento", movement.sqrMagnitude > 0 ? 1 : 0);
+            anim.SetLayerWeight(1, 0);
+        }
+
+        if (movement != Vector2.zero)
+        {
+            anim.SetBool("walking", true);
+        }
+        else
+        {
+            anim.SetBool("walking", false);
         }
     }
 
-    void Flip()
+    private void ResetLayers()
     {
-        if (movement.x > 0)
-        {
-            transform.eulerAngles = new Vector3(0f, 0f, 0f);
-        }
-        else if (movement.x < 0)
-        {
-            transform.eulerAngles = new Vector3(0f, 180f, 0f);
-        }
-    }
-
-    void OnAttack()
-    {
-        if (Input.GetMouseButtonDown(0)) // Botão esquerdo do mouse
-        {
-            isAttack = true;
-            moveSpeed = 0;
-        }
-
-        if (Input.GetMouseButtonUp(0))
-        {
-            isAttack = false;
-            moveSpeed = playerInitialSpeed;
-        }
+        anim.SetLayerWeight(0, 0);
+        anim.SetLayerWeight(2, 0);
+        // Corrigido: Removida a linha duplicada
     }
 }
