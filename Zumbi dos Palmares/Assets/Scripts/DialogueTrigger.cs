817fb9ad7cb240f401eedb3e8a91b25b1151ac28
Schlagel <e27544208@gmail.com>
2025-06-16 16:35:51 -0300

    missão arrumada

--- Zumbi dos Palmares/Assets/Scripts/DialogueTrigger.cs
+++ Zumbi dos Palmares/Assets/Scripts/DialogueTrigger.cs
@@ -2,35 +2,43 @@ using UnityEngine;
 
 public class DialogueTrigger : MonoBehaviour
 {
-    public Dialogue dialogue;
+    public Dialogue dialogueAntesCenouras;
+    public Dialogue dialogueDepoisCenouras;
+
     private bool playerInRange = false;
+    private MissaoManager missaoManager;
+
+    void Start()
+    {
+        missaoManager = FindObjectOfType<MissaoManager>();
+    }
 
     void Update()
     {
         if (playerInRange && Input.GetKeyDown(KeyCode.E))
         {
-            TriggerDialogue();
-        }
-    }
+            int etapa = missaoManager.etapaMissao;
 
-    public void TriggerDialogue()
-    {
-        FindObjectOfType<DialogueManager>()?.StartDialogue(dialogue);
+            if (etapa == 0)
+            {
+                FindObjectOfType<DialogueManager>()?.StartDialogue(dialogueAntesCenouras);
+                missaoManager.FalouComBarao();
+            }
+            else if (etapa == 2)
+            {
+                FindObjectOfType<DialogueManager>()?.StartDialogue(dialogueDepoisCenouras);
+                missaoManager.FalouComBarao();
+            }
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
-        {
-            playerInRange = true;
-        }
+        if (other.CompareTag("Player")) playerInRange = true;
     }
 
     void OnTriggerExit2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
-        {
-            playerInRange = false;
-        }
+        if (other.CompareTag("Player")) playerInRange = false;
     }
 }
