96682620ea0cfe82013cf80a0a7b21162e858f3e
PhelipeTamagno <phelipetamagno@gmail.com>
2025-06-17 21:43:24 -0300

    f

--- Zumbi dos Palmares/Assets/Luzes/Luz2D.cs
+++ Zumbi dos Palmares/Assets/Luzes/Luz2D.cs
@@ -1,16 +1,35 @@
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
+[RequireComponent(typeof(Light2D))]
 public class Luz2D : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Light2D fireLight;
+
+    [Header("Intensity Flicker")]
+    public float minIntensity = 0.8f;
+    public float maxIntensity = 1.2f;
+
+    [Header("Radius Flicker")]
+    public float minRadius = 3.5f;
+    public float maxRadius = 4.5f;
+
+    [Header("Flicker Speed")]
+    public float flickerSpeed = 0.1f;
+
+    private void Awake()
     {
-        
+        fireLight = GetComponent<Light2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        InvokeRepeating("Flicker", 0f, flickerSpeed);
+    }
+
+    void Flicker()
+    {
+        fireLight.intensity = Random.Range(minIntensity, maxIntensity);
+        fireLight.pointLightOuterRadius = Random.Range(minRadius, maxRadius);
     }
 }
