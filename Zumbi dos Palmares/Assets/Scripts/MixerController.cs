d5effc9cfdaa6eaaa41abea6f9e75cddd50a7c50
PhhelipeTamagno <phelipetamagno@gmail.com>
2025-07-03 14:16:29 -0300

    oi

--- Zumbi dos Palmares/Assets/Scripts/MixerController.cs
+++ Zumbi dos Palmares/Assets/Scripts/MixerController.cs
@@ -1,16 +1,22 @@
 using UnityEngine;
 using UnityEngine.Audio;
-using System.Collections;
-using System.Collections.Generic;
 
 public class MixerController : MonoBehaviour
 {
     [SerializeField] private AudioMixer myAudioMixer;
 
+    void Start()
+    {
+        float savedVolume = PlayerPrefs.GetFloat("MasterVolumeValue", 0.75f);
+        SetVolume(savedVolume);
+    }
+
     public void SetVolume(float value)
     {
-        // Evita erro de logaritmo com zero
-        float volume = Mathf.Log10(Mathf.Clamp(value, 0.0001f, 1f)) * 20f;
-        myAudioMixer.SetFloat("MasterVolume", volume);
+        float volumeInDb = Mathf.Log10(Mathf.Clamp(value, 0.0001f, 1f)) * 20f;
+        myAudioMixer.SetFloat("MasterVolume", volumeInDb);
+
+        // Salva o valor para outras cenas
+        PlayerPrefs.SetFloat("MasterVolumeValue", value);
     }
 }
